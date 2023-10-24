7930933e30d84e62a1c12422b1f9c813c8eb8ce0
SarKetch <>
2023-10-24 16:02:51 +0000

    Add first section of java code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("id") === "submit-btn") {
+                displayRecipes(); 
+            } else if (this.classList.contains("round-button")) {
+                selectMood(this);
+            } else if (this.getAttribute("id") === "refresh-btn") {
+                location.reload();  
+            }
+        });
+    }
+
